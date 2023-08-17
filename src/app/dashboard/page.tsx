ba0205a0226c1a9c928396c7a1ac668c3dad19cb
Nayanp35 <prajapatinayan35@gmail.com>
2023-08-16 23:42:49 -0400

    init_dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 import HeaderText from "../components/HeaderText";
+import CardLineChart from "../components/charts/LineGraph";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="h-screen">
       <HeaderText text="Dashboard" />
+      <div className="flex flex-wrap">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <CardLineChart />
+        </div>
+        <div className="w-full xl:w-4/12 px-4">{/* <CardBarChart /> */}</div>
+      </div>
+      <div className="flex flex-wrap mt-4">
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          {/* <CardPageVisits /> */}
+        </div>
+        <div className="w-full xl:w-4/12 px-4">
+          {/* <CardSocialTraffic /> */}
+        </div>
+      </div>
     </div>
   );
 }
