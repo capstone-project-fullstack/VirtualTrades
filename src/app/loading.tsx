b70205a93f8dee9e84b463d0ac00067bebef8a59
Nayanp35 <prajapatinayan35@gmail.com>
2023-08-21 02:43:58 -0400

    enhanced trading form

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import Loading from "@/app/components/Loading";
+
+export default function Load() {
+  return <Loading />;
+}
--- src/app/stock/[ticker]/TradeForm.tsx
+++ src/app/stock/[ticker]/TradeForm.tsx
@@ -1,182 +1,56 @@
-/* eslint-disable no-unused-vars */
-"use client";
+import Stock from "../../modals/stock";
+import TradeStocks from "@/app/modals/tradeStocks";
+import { currentUser } from "@clerk/nextjs";
+import Portfolio from "../../modals/portfolio";
+import TradeFormComponent from "./TradeFormComponent";
+import { revalidatePath } from "next/cache";
 
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Input,
-  Button,
-  Typography,
-  Tabs,
-  TabsHeader,
-  TabsBody,
-  Tab,
-  TabPanel,
-} from "@material-tailwind/react";
-import { defaultValue } from "@material-tailwind/react/types/components/slider";
+const TradeForm = async ({ ticker }: { ticker: string }) => {
+  const currUser = await currentUser();
+  if (!currUser) return <div></div>;
+  const userId = currUser.id;
 
-import { useState } from "react";
+  let stock = await Stock.findStockIfExist(ticker);
 
-interface TradeFormProps {
-  price: number;
-  sharesOwned: number;
-  buyStock: (data: FormData) => void;
-  sellStock: (data: FormData) => void;
-  buyingPower: number;
-}
+  let buyingPower = await Portfolio.getBuyingPower(userId);
 
-export default function TradeForm({
-  price,
-  sharesOwned,
-  buyStock,
-  sellStock,
-  buyingPower,
-}: TradeFormProps) {
-  const [type, setType] = useState("buy");
-  const [buyShares, setBuyShares] = useState(0);
-  const [sellShares, setSellShares] = useState(0);
-
-  const formatPrice = (price: number) => {
-    return price.toLocaleString("en-US", {
-      style: "currency",
-      currency: "USD",
-    });
+  if (!stock) {
+    const createdStock = await Stock.createStockIfNotExist(ticker);
+    if (!createdStock) return <div>No Stock Found</div>;
+    else stock = createdStock;
   }
 
+  const isSellable = await TradeStocks.isSellable(userId, stock.id);
+  const shareOwned = isSellable.length ? isSellable[0].shares : 0;
+  const currentPrice = await Stock.getCurrentPrice(ticker);
+
+  const buyStock = async (data: FormData) => {
+    "use server";
+    const shares = data.get("sharesToBuy")?.valueOf();
+    if (!shares || !stock || !stock.id) return;
+    await TradeStocks.buyStock(userId, stock.id, Number(shares));
+    revalidatePath("/");
+  };
+
+  const sellStock = async (data: FormData) => {
+    "use server";
+    const shares = data.get("sharesToSell")?.valueOf();
+    if (!shares || !stock || !stock.id) return;
+    await TradeStocks.sellStock(userId, stock.id, Number(shares));
+    revalidatePath("/");
+  };
+
   return (
-    <Card className="w-full max-w-[24rem]">
-      <CardHeader
-        color="gray"
-        floated={false}
-        shadow={false}
-        className="m-0 grid place-items-center rounded-b-none py-3 px-4 text-center"
-      >
-        <Typography variant="h4" color="white">
-          Trade Stock
-        </Typography>
-      </CardHeader>
-      <CardBody>
-        <Tabs value={type} className="overflow-visible">
-          <TabsHeader className="relative z-0 ">
-            <Tab value="buy" onClick={() => setType("buy")}>
-              Buy
-            </Tab>
-            <Tab
-              value="sell"
-              onClick={() => setType("sell")}
-              disabled={sharesOwned <= 0}
-            >
-              Sell
-            </Tab>
-          </TabsHeader>
-          <TabsBody
-            className="!overflow-x-hidden !overflow-y-visible"
-            animate={{
-              initial: {
-                x: type === "buy" ? 400 : -400,
-              },
-              mount: {
-                x: 0,
-              },
-              unmount: {
-                x: type === "buy" ? 400 : -400,
-              },
-            }}
-          >
-            <TabPanel value="buy" className="p-0">
-              <form action={buyStock} className="mt-6 flex flex-col gap-4">
-                <div>
-                  <Input
-                    label="Number of Shares"
-                    type="number"
-                    containerProps={{ className: "min-w-[72px]" }}
-                    crossOrigin="anonymous"
-                    name="sharesToBuy"
-                    onChange={(e) => setBuyShares(Number(e.target.value))}
-                    min={1}
-                    max={Math.floor(buyingPower / price)}
-                    required
-                  />
-                </div>
-
-                <div className="my-2">
-                  <Typography
-                    variant="h5"
-                    color="blue-gray"
-                    className="mb-2 font-medium"
-                  >
-                    Details
-                  </Typography>
-                  <div className="border-y border-black">
-                    Quantity: <span className="float-right">{buyShares}</span>
-                  </div>
-                  <div className="border-b border-black">
-                    Buying Power:{" "}
-                    <span className="float-right">{formatPrice(buyingPower)}</span>
-                  </div>
-                  <div className="border-b border-black">
-                    Cost per Share: <span className="float-right">{formatPrice(price)}</span>
-                  </div>
-                  <div className="border-b border-black">
-                    Total:{" "}
-                    <span className="float-right">{formatPrice(price * buyShares)}</span>
-                  </div>
-                </div>
-                <Button type="submit" size="lg">
-                  Buy
-                </Button>
-              </form>
-            </TabPanel>
-            <TabPanel value="sell" className="p-0">
-              <form action={sellStock} className="mt-6 flex flex-col gap-4">
-                <div>
-                  <Input
-                    label="Number of Shares"
-                    type="number"
-                    containerProps={{ className: "min-w-[72px]" }}
-                    crossOrigin="anonymous"
-                    name="shareToSell"
-                    onChange={(e) => setSellShares(Number(e.target.value))}
-                    min={1}
-                    max={sharesOwned}
-                    required
-                  />
-                </div>
-
-                <div className="my-2">
-                  <Typography
-                    variant="h5"
-                    color="blue-gray"
-                    className="mb-2 font-medium"
-                  >
-                    Details
-                  </Typography>
-                  <div className="border-y border-black">
-                    Quantity: <span className="float-right">{sellShares}</span>
-                  </div>
-                  <div className="border-b border-black">
-                    Buying Power:{" "}
-                    <span className="float-right">{formatPrice(buyingPower)}</span>
-                  </div>
-                  <div className="border-b border-black">
-                    Cost per Share: <span className="float-right">{formatPrice(price)}</span>
-                  </div>
-                  <div className="border-b border-black">
-                    Total:{" "}
-                    <span className="float-right">{formatPrice(price * sellShares)}</span>
-                  </div>
-
-                  <div className="my-4 flex items-center gap-4"></div>
-                </div>
-                <Button type="submit" size="lg">
-                  Sell
-                </Button>
-              </form>
-            </TabPanel>
-          </TabsBody>
-        </Tabs>
-      </CardBody>
-    </Card>
+    <div className="w-full">
+      <TradeFormComponent
+        sharesOwned={shareOwned}
+        price={currentPrice}
+        buyStock={buyStock}
+        sellStock={sellStock}
+        buyingPower={buyingPower}
+      />
+    </div>
   );
-}
+};
+
+export default TradeForm;
