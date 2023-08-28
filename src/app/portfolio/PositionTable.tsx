c8cc7dcda8201c26f5ba61c55c8e53265e28fab5
Nayanp35 <prajapatinayan35@gmail.com>
2023-08-28 14:39:16 -0400

    abstracted away the custom sort function

--- src/app/portfolio/PositionTable.tsx
+++ src/app/portfolio/PositionTable.tsx
@@ -14,7 +14,7 @@ import {
 } from '@material-tailwind/react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { formatPrice } from '../utils/utils';
+import { formatPrice, customSort } from '../utils/utils';
 
 interface PortfolioData {
   shares: number;
@@ -32,7 +32,7 @@ interface PortfolioData {
 export default function PositionTable() {
   const [tableRows, setTableRows] = useState<PortfolioData[]>([]);
   const [searchStock, setSearchStock] = useState<string>('');
-  const [sortOrder, setSortOrder] = useState<'asc' | 'desc' | null>(null);
+  const [sortOrder, setSortOrder] = useState<'asc' | 'desc' | "">("");
 
   useEffect(() => {
     axios
@@ -50,29 +50,6 @@ export default function PositionTable() {
     );
   });
 
-  const customSort = (sortBy: string) => {
-    const isAscending = sortOrder === 'asc';
-    filterStock.sort((a, b) => {
-      let comparison = 0;
-      if (sortBy === 'symbol') {
-        comparison = a.Stock.symbol.localeCompare(b.Stock.symbol);
-      } else if (sortBy === 'price') {
-        comparison = a.Stock.current_price - b.Stock.current_price;
-      } else if (sortBy === 'gain') {
-        comparison = a.gain - b.gain;
-      } else if (sortBy === 'averagePrice') {
-        comparison = a.average_price - b.average_price;
-      } else if (sortBy === 'totalEquity') {
-        comparison = a.total_equity - b.total_equity;
-      } else if (sortBy === 'shares') {
-        comparison = a.shares - b.shares;
-      }
-
-      return isAscending ? comparison : -comparison; // Reverse the comparison if descending
-    });
-
-    setTableRows([...filterStock]);
-  };
 
   const tableHead = [
     { header: 'Symbol', sortKey: 'symbol' },
@@ -136,7 +113,7 @@ export default function PositionTable() {
                       } else {
                         setSortOrder('desc');
                       }
-                      customSort(head.sortKey);
+                      customSort(head.sortKey, tableRows, setTableRows, sortOrder);
                     }}
                   >
                     {head.header}
