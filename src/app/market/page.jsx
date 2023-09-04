38beed34c4ee8bd44dd8634118b7e050bfa70b53
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-04 00:02:27 -0400

    better responsive for mid size width

--- src/app/market/page.jsx
+++ src/app/market/page.jsx
@@ -13,19 +13,19 @@ const Market = () => {
       <TopWidget />
       <HeaderText text="Market" />
       <div className="f-center h-full">
-        <div className="w-full md:w-[90%] h-full">
-          <div className="flex flex-col md:flex-row gap-3 mx-3 mt-3 md:m-0">
-            <div className="flex-auto md:max-w-[65%]">
+        <div className="w-full lg:w-[90%] h-full">
+          <div className="flex flex-col lg:flex-row gap-3 mx-3 mt-3 lg:m-0">
+            <div className="flex-auto lg:max-w-[65%]">
               <Heatmap />
             </div>
-            <div className="md:min-w-[35%] flex flex-col sm:justify-center">
+            <div className="lg:min-w-[35%] flex flex-col lg:justify-center">
               <MarketOverviewWidget />
             </div>
           </div>
         </div>
       </div>
       <div className="f-center h-full">
-        <div className="m-3 w-full md:w-[90%]">
+        <div className="m-3 w-full lg:w-[90%]">
           <ScreenerWidget />
         </div>
       </div>
