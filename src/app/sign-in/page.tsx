db8babc802200ea96e4bf3f93ac3a834972073b0
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-03 21:39:14 -0400

    center the widget

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,8 +2,10 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function Page() {
   return (
-    <div className="text-white flex justify-center items-center">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+    <div className="h-[70vh]">
+      <div className="f-center h-full">
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      </div>
     </div>
   );
 }
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,24 +1,10 @@
 import { SignUp } from '@clerk/nextjs';
-// import { prisma } from "../../../lib/prisma";
-// import { auth } from '@clerk/nextjs';
-
-// async function createUser(userId: string) {
-//   // return prisma
-//   const user = await prisma.user.create({
-//     data: {
-//       id: userId,
-//       initial_amount: 0,
-//       cash: 0,
-//       current_portfolio_value: 0,
-//     },
-//   });
-//   return user;
-// }
-
 export default function Page() {
   return (
-    <div className="text-white flex justify-center items-center">
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+    <div className="h-[80vh]">
+      <div className="f-center h-full">
+        <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+      </div>
     </div>
   );
 }
