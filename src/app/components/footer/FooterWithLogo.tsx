905d425b5f05f205c9d35d96184fa43cd8c41ace
Magdalena May Gero <114107908+magdalenamaygero@users.noreply.github.com>
2023-09-07 12:00:31 -0400

    functional and styled footer complete

--- src/app/components/footer/FooterWithLogo.tsx
+++ src/app/components/footer/FooterWithLogo.tsx
@@ -1,20 +1,36 @@
 'use client';
 
 import { Typography } from '@material-tailwind/react';
+import { useRouter } from 'next/navigation';
 
-export default function FooterWithLogo() {
+interface BlogCardProps {
+  // textButton: any;
+  onClick: () => void;
+}
+
+export default function FooterWithLogo({
+  // textButton,
+  onClick,
+}: BlogCardProps) {
+
+  const router = useRouter();
+
+  const handleRoute = (page: string) => {
+    router.push(`/${page}`);
+  };
+
   return (
     <>
       <footer className="ml-16 p-8">
-        <div className="flex flex-row  flex-wrap items-center justify-center gap-y-6 gap-x-12 text-center md:justify-between">
-          {/* <Img src="/img/logo-ct-dark.png" alt="logo-ct" className="w-10" /> */}
-          <ul className="flex flex-wrap  items-center gap-y-2 gap-x-8">
+        <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 text-center md:justify-between">
+          <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
             <li>
               <Typography
                 as="a"
                 href="#"
                 color="blue-gray"
-                className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500 text-white"
+                className="font-normal transition-colors hover:text-light-green focus:text-light-green text-white"
+                onClick={() => handleRoute('portfolio')}
               >
                 Portfolio
               </Typography>
@@ -24,7 +40,8 @@ export default function FooterWithLogo() {
                 as="a"
                 href="#"
                 color="blue-gray"
-                className="font-normal transition-colors text-white hover:text-blue-500 focus:text-blue-500"
+                className="font-normal transition-colors hover:text-light-green focus:text-light-green text-white"
+                onClick={() => handleRoute('market')}
               >
                 Stock Market
               </Typography>
@@ -34,8 +51,9 @@ export default function FooterWithLogo() {
                 as="a"
                 href="#"
                 color="blue-gray"
-                className="font-normal transition-colors text-white hover:text-blue-500 focus:text-blue-500"
-              >
+                className="font-normal transition-colors hover:text-light-green focus:text-light-green text-white"
+                onClick={() => handleRoute('watchlists')}    
+                      >
                 Watchlist
               </Typography>
             </li>
@@ -44,8 +62,8 @@ export default function FooterWithLogo() {
                 as="a"
                 href="#"
                 color="blue-gray"
-                className="font-normal transition-colors text-white hover:text-blue-500 focus:text-blue-500"
-              >
+                className="font-normal transition-colors hover:text-light-green focus:text-light-green text-white"
+                onClick={() => handleRoute('news')}               >
                 News
               </Typography>
             </li>
