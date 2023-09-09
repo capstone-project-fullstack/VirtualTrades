fd0837f6bbf970500eb8b7353573eb1da23311cc
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-09 02:18:37 -0400

    Styles for buttons

--- src/app/news/PaginationButtons.tsx
+++ src/app/news/PaginationButtons.tsx
@@ -19,12 +19,11 @@ const PaginationButtons: React.FC<PaginationButtonsProps> = ({
   onNextClick,
 }) => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2 ">
       <Button
         onClick={onPrevClick}
-        className="flex items-center gap-2"
+        className="flex items-center gap-2 bg-light-green"
         disabled={active === 1}
-        color="blue"
       >
         <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" /> Previous
       </Button>
@@ -33,7 +32,7 @@ const PaginationButtons: React.FC<PaginationButtonsProps> = ({
           <IconButton
             key={index}
             {...(active === index + 1
-              ? { variant: 'filled', color: 'blue' }
+              ? { variant: 'filled', className: 'bg-none bg-light-green' }
               : { variant: 'text', color: 'white' })}
             onClick={() => onItemClick(index + 1)}
           >
@@ -43,9 +42,8 @@ const PaginationButtons: React.FC<PaginationButtonsProps> = ({
       </div>
       <Button
         onClick={onNextClick}
-        className="flex items-center gap-2"
+        className="flex items-center gap-2 bg-light-green"
         disabled={active === total}
-        color="blue"
       >
         Next <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
       </Button>
