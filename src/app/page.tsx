4ed8ae0f50c5ec8bd27febd3c7735e52ef4b1bc1
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-09 02:23:46 -0400

    Landing page styles w/ Bayzad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,20 +19,19 @@ export default function Home() {
   return (
     <div>
       <TradingViewWidget />
-      <div className="leading-normal tracking-normal text-white gradient">
+      <div className="leading-normal tracking-normal text-white gradient my-20">
         <div className="m-8 f-center">
-          <div className="container f-center flex-wrap flex-col lg:flex-row">
-            <div className="flex flex-col w-full lg:w-1/2 justify-center items-start text-center md:text-left">
-              <h1 className="my-4 text-5xl font-bold leading-tight">
-                Welcome to{' '}
-                <span className="text-light-green">VirtualTrades</span>
+          <div className="container f-center gap-x-5 flex-wrap flex-col lg:flex-row">
+            <div className="flex flex-col w-full lg:w-1/3 justify-center items-start text-center md:text-left">
+              <h1 className="my-2 text-6xl font-bold leading-[1]">
+                Welcome to <div className="text-light-green">VirtualTrades</div>
               </h1>
-              <p className="leading-normal text-3xl mt-4 mb-8">
+              <p className="leading-normal text-xl mt-2 mb-4">
                 Learn to interpret financial indicators, charts and graphs to
                 make better informed decisions at the start of your investment
                 journey.
               </p>
-              <p className="leading-normal text-2xl mb-8">
+              <p className="leading-normal text-xl mb-8">
                 Ready to dive in? Welcome to the world of trading stocks with no
                 financial risk. Here, you can buy and sell virtual stock at real
                 market prices, every day!
@@ -60,28 +59,23 @@ export default function Home() {
               <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
             </div>
             <div className="flex flex-wrap justify-center">
-              <HorizontalCard
-                img="https://www.nyse.com/publicdocs/images/NYSE_Trading_Floor_New_Branding.jpg"
-                text1="Sign up now to receive real-time overviews"
-                text2="Welcome to our Stock Market, where you can explore real-time trends and insights. An array of interactive graphs showcasing live stock data allows you to track the performance of various stocks and delve into comprehensive analyses."
-                text3="Examine trends and learn to invest with confidence!"
-                onClick={() => handleRoute('watchlists')}
-              />
+              <HorizontalCard img="https://www.nyse.com/publicdocs/images/NYSE_Trading_Floor_New_Branding.jpg" />
             </div>
           </div>
         </div>
       </section>
 
-      <div className="relative -mt-12 lg:-mt-24">
+      <div className="relative -mt-12 lg:-mt-[70px]">
         <svg
           viewBox="0 0 1428 174"
           version="1.1"
           xmlns="http://www.w3.org/2000/svg"
+          transform="scale(1, -1)" // Add this transform attribute
         >
           <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
             <g
               transform="translate(-2.000000, 44.000000)"
-              fill="#FFFFFF"
+              fill="#6129d1"
               fillRule="nonzero"
             >
               <path
@@ -100,7 +94,7 @@ export default function Home() {
             </g>
             <g
               transform="translate(-4.000000, 76.000000)"
-              fill="dark-grey"
+              fill="#35007A"
               fillRule="nonzero"
             >
               <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path>
