32f531fef526034a2ed64e98371ca4f6b5d77ae7
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-10 19:56:18 -0400

    Make it render exactly in the center

--- src/app/components/loading/Loading.tsx
+++ src/app/components/loading/Loading.tsx
@@ -4,7 +4,7 @@ import './index.css';
 
 export default function Loading() {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="fixed top-1/2 left-1/2 -translate-x-[37%] -translate-y-1/2">
       <div className="svg-frame">
         <svg>
           <g id="out1">
