21657f4a99020fa912a17c82c4af846715f5bc78
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-11 18:27:04 -0400

    Add tooltip info

--- src/app/portfolio/Funds.tsx
+++ src/app/portfolio/Funds.tsx
@@ -3,15 +3,24 @@ import { useState } from 'react';
 import FundsManagementForm from './FundsManagementForm';
 import { formatPrice } from '../utils/utils';
 import { useAppSelector } from '../redux/hooks';
+import Tooltip from '../components/Tooltip';
 
 export default function Funds() {
   const [open, setOpen] = useState(false);
   const funds = useAppSelector((state) => state.fundManagement.values);
 
   return (
-    <div className="h-28 flex justify-center sm:justify-start sm:items-center">
+    <div className="h-20 md:h-28 flex justify-center sm:justify-start sm:items-center">
       <div>
-        <div>Buying Power</div>
+        <div className="text-center md:text-left flex gap-1 mt-2 md:m-0">
+          Buying Power
+          <span className="mt-[2px]">
+            <Tooltip
+              title="Available Funds"
+              text="Amount of cash available for trading"
+            />
+          </span>
+        </div>
         <div className="w-fit flex flex-row gap-x-3">
           <div className="text-white text-3xl font-semibold">
             {formatPrice(funds?.cash || 0)}
