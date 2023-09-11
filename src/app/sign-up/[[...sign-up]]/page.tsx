42f0aa802eb6e24a7191d9164bb3acf26fe73d91
Nayanp35 <prajapatinayan35@gmail.com>
2023-09-11 03:35:36 -0400

    Allow to enter more info

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignUp } from '@clerk/nextjs';
 export default function Page() {
   return (
-    <div className="h-[80vh]">
+    <div className="h-screen">
       <div className="f-center h-full">
         <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
       </div>
